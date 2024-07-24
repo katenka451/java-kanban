aece7600210d5493891cf86332934900f5403c61
katenka451 <pozor451@yandex.ru>
2024-07-24 16:45:28 +0300

    sprint6/dev tasks tracker

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        //По ТЗ неясно, нужно ли оставлять код для тестирования. Решила оставить
         TaskManager manager = Managers.getDefault();
 
         Task task1 = new Task(
@@ -29,52 +28,39 @@ public class Main {
                 "Test subtask 1.2",
                 epic1.getId());
         manager.createSubtask(subtask1_2);
+        Subtask subtask1_3 = new Subtask(
+                "Subtask 1.3",
+                "Test subtask 1.3",
+                epic1.getId());
+        manager.createSubtask(subtask1_3);
 
         Epic epic2 = new Epic(
                 "Epic2",
                 "Test epic2");
         manager.createEpic(epic2);
-        Subtask subtask2_1 = new Subtask(
-                "Subtask 2.1",
-                "Test subtask 2.1",
-                epic2.getId());
-        manager.createSubtask(subtask2_1);
 
-        printAllTasks(manager);
-
+        Task findEpic1 = manager.getEpicById(epic1.getId());
         Task findTask2 = manager.getTaskById(task2.getId());
-        Task findSubtask1 = manager.getSubtaskById(subtask1_1.getId());
+        Task findSubtask1_1 = manager.getSubtaskById(subtask1_1.getId());
         Task findEpic2 = manager.getEpicById(epic2.getId());
         findEpic2 = manager.getEpicById(epic2.getId());
+        Task findSubtask1_3 = manager.getSubtaskById(subtask1_3.getId());
         findEpic2 = manager.getEpicById(epic2.getId());
-        findEpic2 = manager.getEpicById(epic2.getId());
+        Task findSubtask1_2 = manager.getSubtaskById(subtask1_2.getId());
         findEpic2 = manager.getEpicById(epic2.getId());
         findEpic2 = manager.getEpicById(epic2.getId());
         Task findTask1 = manager.getTaskById(task1.getId());
         findTask1 = manager.getTaskById(task1.getId());
+        findEpic2 = manager.getEpicById(epic2.getId());
+        findSubtask1_3 = manager.getSubtaskById(subtask1_3.getId());
         findTask2 = manager.getTaskById(task2.getId());
+        printHistory(manager);
 
-        printAllTasks(manager);
+        manager.deleteSubtaskById(subtask1_3.getId());
+        printHistory(manager);
 
-        task1.setTaskStatus(Status.IN_PROGRESS);
-        manager.updateTask(task1);
-
-        subtask1_1.setTaskStatus(Status.DONE);
-        manager.updateSubtask(subtask1_1);
-
-        System.out.println("------------------");
-        printAllTasks(manager);
-
-        manager.deleteSubtaskById(subtask1_1.getId());
-
-        System.out.println("------------------");
-        printAllTasks(manager);
-
-        manager.deleteTaskById(task2.getId());
-        manager.deleteEpicById(epic2.getId());
-
-        System.out.println("------------------");
-        printAllTasks(manager);
+        manager.deleteEpicById(epic1.getId());
+        printHistory(manager);
 
     }
 
@@ -94,6 +80,15 @@ public class Main {
                         " (" + subtask.getTaskDescription() + "), статус - " + subtask.getTaskStatus());
             }
         }
+        System.out.println("------------------");
+        System.out.println("История:");
+        for (Task task : manager.getHistory()) {
+            System.out.println(task.getTaskName());
+        }
+        System.out.println("------------------");
+    }
+
+    static void printHistory(TaskManager manager) {
         System.out.println("------------------");
         System.out.println("История:");
         for (Task task : manager.getHistory()) {
--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -28,7 +28,7 @@ class InMemoryHistoryManagerTest {
     }
 
     @Test
-    void isPreviousVersionSaved() {
+    void isTaskPreviousVersionSaved() {
         task.setTaskStatus(Status.IN_PROGRESS);
         historyManager.add(task);
         task.setTaskStatus(Status.DONE);
@@ -37,4 +37,24 @@ class InMemoryHistoryManagerTest {
         assertEquals(Status.IN_PROGRESS, history.getLast().getTaskStatus(), "В историю не сохраняется предыдущая версия данных");
 
     }
+
+    @Test
+    void isTasksUnique() {
+        task.setTaskStatus(Status.IN_PROGRESS);
+        historyManager.add(task);
+        task.setTaskStatus(Status.DONE);
+        historyManager.add(task);
+        List<Task> history = historyManager.getHistory();
+        assertNotNull(history, "История пустая.");
+        assertEquals(history.size(), 1, "В историю сохраняются дубликаты");
+
+        task.setId(1);
+        historyManager.add(task);
+        history = historyManager.getHistory();
+        assertNotNull(history, "История пустая.");
+        assertEquals(history.size(), 2, "В историю не сохраняются задачи с разными ID");
+
+        assertNotEquals(history.get(0).getId(), history.get(1).getId(), "В историю сохраняются ссылки на оригинальные объекты");
+    }
+
 }
