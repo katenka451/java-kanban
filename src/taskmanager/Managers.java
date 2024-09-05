f974266ff05806e8c67da79a22faf7dcc4729614
katenka451 <pozor451@yandex.ru>
2024-09-05 15:58:00 +0300

    sprint9/dev tasks tracker (codestyle fix v3)

--- src/taskmanager/Managers.java
+++ src/taskmanager/Managers.java
@@ -7,10 +7,12 @@ public class Managers {
     private static final String FILE_PATH = "kanban_backup.csv";
 
     public static TaskManager getDefault() {
-       return new InMemoryTaskManager();
+        return new InMemoryTaskManager();
     }
 
-    public static FileBackedTaskManager getFileBackedManager() { return FileBackedTaskManager.loadFromFile(Paths.get(FILE_PATH)); }
+    public static FileBackedTaskManager getFileBackedManager() {
+        return FileBackedTaskManager.loadFromFile(Paths.get(FILE_PATH));
+    }
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
