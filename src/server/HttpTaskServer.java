f2bd557e2c783c247d83ce83c4fafca4d68f3c8c
katenka451 <pozor451@yandex.ru>
2024-09-05 15:56:23 +0300

    sprint9/dev tasks tracker (codestyle fix v2)

--- src/server/HttpTaskServer.java
+++ src/server/HttpTaskServer.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 
 public class HttpTaskServer {
 
-    private final int PORT = 8080;
+    private static final int PORT = 8080;
     private final TaskManager taskManager;
     private final Gson gson;
     private final HttpServer httpServer;
